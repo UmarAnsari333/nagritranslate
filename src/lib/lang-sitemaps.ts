c94bea0355bfee4e3eadcecf64a27d606e03a344
umarkhursheed314 <umar.khursheed314@gmail.com>
2026-04-25 14:57:04 +0500

    bisaya added

--- src/lib/lang-sitemaps.ts
+++ src/lib/lang-sitemaps.ts
@@ -3,4 +3,9 @@
 // in the sitemap index and generate its dedicated /lang-sitemap/[slug].xml
 // covering all [lang]-to-[every other language] pairs.
 
-export const PER_LANGUAGE_SITEMAPS = ["Ilocano", "Tagalog", "Filipino"];
+export const PER_LANGUAGE_SITEMAPS = [
+  "Ilocano",
+  "Tagalog",
+  "Filipino",
+  "Bisaya",
+];
